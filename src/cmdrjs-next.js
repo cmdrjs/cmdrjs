549af4486181470411927a3ceb10065cbd0bf60a
cruikshj <cruikshank@Cruikshank10.zywave.com>
2015-12-21 19:33:42 -0600

    next

---
+++ src/cmdrjs-next.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+var defaultSettings = {
+    
+}
+
+class Cmdr {
+    constructor(element, settings) {
+        
+    }
+}
+
+Cmdr.version = '1.1.0-alpha';
+
+export default Cmdr;
